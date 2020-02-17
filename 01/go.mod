a2c6c34a81bfdf35e6af9fdcb7390059c8574ab7
yohei-takara <b195.clo5566@gmail.com>
2020-02-17 23:26:28 +0900

    token, lexer

---
+++ 01/go.mod
@@ -0,0 +1,3 @@
+module yohei-takara/stary-writing-an-interpreter-in-go/01
+
+go 1.13
---
+++ 01/lexer/lexer.go
@@ -0,0 +1,23 @@
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int  // 入力における現在の位置（現在の文字を指し示す）
+	readPosition int  // これから読み込む位置（現在の文字の次）
+	ch           byte // 現在検査中の文字
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
